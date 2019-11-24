db59dcb86dd4ad26cb7a31b1bf4c1a87a7005980
KOOL <trantrilong318@gmail.com>
2019-11-24 21:05:29 +0700

    fix class  router

--- lib/router.rb
+++ lib/router.rb
@@ -3,4 +3,17 @@ class Router
     '/' => 'WelcomeController#index',
     '/users' => 'UsersController#index'
   }
+
+  def call(env)
+    if (mapping = ROUTES[env['PATH_INFO']])
+            env.merge!(controller_action(mapping))
+            app.call(env)
+    else
+      Rack::Response.new('Not found', 404)
+    end
+  end
+
+  def controller_action(mapping)
+     Hash[%w(controller action).zip mapping.split('#')]
+  end
 end
